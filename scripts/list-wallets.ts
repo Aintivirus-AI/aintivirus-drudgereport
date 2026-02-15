67a772b426232687d9be4591ad72ec9d2df59c53
Chandler Phillips <chandler@chandlerphillips.net>
2026-02-15 11:27:32 -0600

    changes

--- scripts/list-wallets.ts
+++ scripts/list-wallets.ts
@@ -8,7 +8,7 @@
  */
 
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: ".env.local" });
 
 import { getAllTokens } from "../lib/db";
 import { decryptPrivateKey } from "../lib/secrets-provider";
